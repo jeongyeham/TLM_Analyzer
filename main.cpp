8203ad719befa32c2c9104403795ce00f646b734
jeong <jeongyeham@foxmail.com>
2025-10-27 09:36:36 +0800

    feat(ui): set application window icon
    - Added QIcon to set the window icon for the application
    - Loaded icon from resource path ":/images/app.ico"- Applied icon using QApplication::setWindowIcon method

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    
+    QApplication::setWindowIcon(QIcon(":/images/app.ico"));
+
     MainWindow window;
     window.show();
 
